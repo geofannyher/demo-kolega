7d38c7e961cf8aa5c0117d8052800539c6254026
geofannyher <geofannyher@gmail.co>
2024-07-02 15:03:54 +0700

    update

--- src/pages/admin/admin.layout.tsx
+++ src/pages/admin/admin.layout.tsx
@@ -8,8 +8,6 @@ const AdminLayout = () => {
   const checkAdmin = () => {
     if (isAdmin === "adminkolegahr") {
       navigate("/adminavatara");
-    } else {
-      navigate("/adminlogin");
     }
   };
 
